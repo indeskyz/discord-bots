c8585d45f9e8fe4a78a42a2c8e287ec5f4894097
indeskyz <codishazey@yahoo.ca>
2022-01-29 00:55:16 -0500

    added verify command

---
+++ commands/verify.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('verify')
+        .setDescription('used to verify users'),
+    async execute(interaction){
+        if(interaction.guild.name === 'verify'){
+            await interaction.reply({content: 'Verify yourself by clicking the react button!', fetchReply:true});
+            message.react('✅')
+            .then(()=> console.log(interaction.member))
+        }
+    }
+}
+
