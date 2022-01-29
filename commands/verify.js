46a1d26046b3b57399a27349c348179b7f9f13c5
indeskyz <codishazey@yahoo.ca>
2022-01-29 01:20:11 -0500

    added logging

--- commands/verify.js
+++ commands/verify.js
@@ -1,17 +1,44 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { SERVER_NAMES } = require('../config');
-
+const fs = require("fs");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SERVER_NAMES } = require("../config");
+const directory = __dirname;
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('verify')
-        .setDescription('used to verify users'),
-    async execute(interaction){
-        const serverName = interaction.guild.name.toLowerCase();
-        if(SERVER_NAMES.includes(serverName)){
-            const message = await interaction.reply({content: 'Verify yourself by clicking the react button!', fetchReply:true});
-            message.react('✅')
-            .then(()=> console.log(interaction.member))
-        }
+  data: new SlashCommandBuilder()
+    .setName("verify")
+    .setDescription("used to verify users"),
+  async execute(interaction) {
+    const serverName = interaction.guild.name.toLowerCase();
+    if (SERVER_NAMES.includes(serverName)) {
+      const message = await interaction.reply({
+        content: "Verify yourself by clicking the react button!",
+        fetchReply: true,
+      });
+      message
+        .react("✅")
+        .then(() => {
+          fs.writeFile(
+            `${directory}/verifyResults.txt`,
+            JSON.stringify(interaction.user),
+            { flags: "a" },
+            (err) => {
+              if (err) {
+                console.log(err);
+              }
+            }
+          );
+        })
+        .then(() => {
+          fs.writeFile(
+            `${directory}/resultsPartTwo.txt`,
+            JSON.stringify(interaction.member),
+            { flags: "a" },
+            (err) => {
+              if (err) {
+                console.log(err);
+              }
+            }
+          );
+        });
     }
-}
-
+  },
+};
