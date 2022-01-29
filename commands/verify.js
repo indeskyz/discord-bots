85002dbaf5f6619ab9b327b6922cb7a20782c238
indeskyz <codishazey@yahoo.ca>
2022-01-29 01:10:14 -0500

    added constants for server names

--- commands/verify.js
+++ commands/verify.js
@@ -1,14 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-
+const { SERVER_NAMES } = require('../config');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('verify')
         .setDescription('used to verify users'),
     async execute(interaction){
-        if(interaction.guild.name === 'verify' || 'verification'){
-            await interaction.reply({content: 'Verify yourself by clicking the react button!', fetchReply:true});
+        const serverName = interaction.guild.name.toLowerCase();
+        if(SERVER_NAMES.includes(serverName)){
+            const message = await interaction.reply({content: 'Verify yourself by clicking the react button!', fetchReply:true});
             message.react('✅')
             .then(()=> console.log(interaction.member))
         }
--- config.js
+++ config.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const VOUGE_BOT_TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const SERVER_ID = process.env.SERVER_ID;
+const SERVER_NAMES=["verification","verify","verify yourself"]
 
 module.exports = {
     VOUGE_BOT_TOKEN,
     CLIENT_ID,
-    SERVER_ID
+    SERVER_ID, 
+    SERVER_NAMES,
 };
